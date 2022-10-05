cb17843ee0480d420de43d13069f6afc90f63986
ArushArk <yyyxy4215@163.com>
2022-10-05 12:10:32 +0800

    Site updated: 2022-10-05 12:10:31

--- js/duration.js
+++ js/duration.js
@@ -1,31 +1,29 @@
-!(function() {
-  /** 计时起始时间，自行修改 **/
-  var start = new Date("2022/09/21 00:00:00");
+!(function () {
+  // 前面补零
+  function prefixInteger(num, n) {
+    return (Array(n).join(0) + num).slice(-n);
+  }
+
+  // 计时起始时间，仿照格式自行修改
+  var start = Date.parse("2022-09-10T00:00:00");
 
   function update() {
-    var now = new Date();
-    now.setTime(now.getTime()+250);
-    days = (now - start) / 1000 / 60 / 60 / 24;
-    dnum = Math.floor(days);
-    hours = (now - start) / 1000 / 60 / 60 - (24 * dnum);
-    hnum = Math.floor(hours);
-    if(String(hnum).length === 1 ){
-      hnum = "0" + hnum;
-    }
-    minutes = (now - start) / 1000 /60 - (24 * 60 * dnum) - (60 * hnum);
-    mnum = Math.floor(minutes);
-    if(String(mnum).length === 1 ){
-      mnum = "0" + mnum;
-    }
-    seconds = (now - start) / 1000 - (24 * 60 * 60 * dnum) - (60 * 60 * hnum) - (60 * mnum);
-    snum = Math.round(seconds);
-    if(String(snum).length === 1 ){
-      snum = "0" + snum;
-    }
-    document.getElementById("timeDate").innerHTML = "本站安全运行&nbsp"+dnum+"&nbsp天";
-    document.getElementById("times").innerHTML = hnum + "&nbsp小时&nbsp" + mnum + "&nbsp分&nbsp" + snum + "&nbsp秒";
+    var now = Date.now() + 250;
+    var interval = (now - start) / 1000;
+    var days = Math.floor(interval / 60 / 60 / 24);
+    var hours = Math.floor((interval / 60 / 60) % 24);
+    var minutes = Math.floor((interval / 60) % 60);
+    var seconds = Math.floor(interval % 60);
+
+    hours = prefixInteger(hours, 2);
+    minutes = prefixInteger(minutes, 2);
+    seconds = prefixInteger(seconds, 2);
+
+    document.getElementById(
+      "website-duration"
+    ).innerHTML = `本站已运行 ${days} 天 ${hours} 小时 ${minutes} 分 ${seconds} 秒`;
   }
 
   update();
-  setInterval(update, 1000);
+  setInterval(update, 250);
 })();
